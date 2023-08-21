eceb840fc9c905d42909edb14d4154e6b853f137
ThomasF85 <thomas.foeldi@gmail.com>
2023-08-21 15:40:03 +0200

    add next auth

--- pages/api/fish/[id].js
+++ pages/api/fish/[id].js
@@ -1,13 +1,25 @@
 import fishData from "@/db/fish.json";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
 
 async function handler(req, res) {
   const { id } = req.query;
   switch (req.method) {
     case "GET":
       try {
+        const session = await getServerSession(req, res, authOptions);
+
+        if (!session) {
+          return res.status(401).json({ message: "Unauthorized" });
+        }
+
         const fish = fishData.find((fish) => fish.id === id);
         if (!fish) {
           return res.status(404).json({ message: "fish not found" });
+        }
+
+        if (session.user.email !== fish.author) {
+          return res.status(401).json({ message: "Unauthorized, wrong" });
         }
         return res.status(200).json(fish);
       } catch (error) {
